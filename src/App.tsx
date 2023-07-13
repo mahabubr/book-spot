8d74551561191ec208cc24b82fdedf6e634cf375
mahabubr <mahabub.r753@gmail.com>
2023-07-13 22:15:37 +0600

    navbar created dione

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import router from "./router/route"
 function App() {
 
   return (
-    <>
+    <div className="container w-11/12 mx-auto">
       <RouterProvider router={router} />
-    </>
+    </div>
   )
 }
 
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar />
           <Outlet />  
           <Footer />
         </div>
