d83f0dc1206b8caebeb99e36b9438da8caccc816
mahabubr <mahabub.r753@gmail.com>
2023-07-13 22:32:55 +0600

    Login Page Design Done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import { FormEvent, useState } from 'react';
 
 const Login = () => {
-    return (
-        <div>
-            Login
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Here, you can perform login logic using email and password state variables
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Reset the form
+    setEmail('');
+    setPassword('');
+  };
+
+  const handleGoogleLogin = () => {
+    // Implement Google login logic here
+    console.log('Perform Google login');
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-8">Login</h1>
+      <form className="bg-white shadow-lg rounded-lg px-12 py-10 w-96" onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-    );
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={handleGoogleLogin}
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
