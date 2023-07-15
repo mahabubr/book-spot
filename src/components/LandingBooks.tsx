177166ad8a064f3a83aba59279b72c49918f23fe
mahabubr <mahabub.r753@gmail.com>
2023-07-15 16:17:26 +0600

    show 10 books on landing page

--- src/components/LandingBooks.tsx
+++ src/components/LandingBooks.tsx
@@ -3,12 +3,12 @@ import { IBooks } from "../types";
 
 const LandingBooks = () => {
 
-    const {data} = useGetBooksQuery(undefined)
+    const {data}= useGetBooksQuery(undefined)
 
     return (
         <div className="my-20 w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
            {
-            data.data.map((book: IBooks) =>  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
+            data.data.slice(0,10).map((book: IBooks) =>  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
             <div className="h-48 bg-gray-800 flex justify-center items-center">
                 <svg
                 className="w-16 h-16 text-white animate-pulse"
@@ -25,10 +25,10 @@ const LandingBooks = () => {
                 </svg>
             </div>
             <div className="py-6 px-4">
-                <h3 className="text-xl font-bold mb-2 text-gray-900">Title</h3>
-                <p className="text-gray-600 mb-2">Author</p>
-                <p className="text-gray-600 mb-2">Genre</p>
-                <p className="text-gray-600">Publication Date</p>
+                <h3 className="text-xl font-bold mb-2 text-gray-900">{book.title}</h3>
+                <p className="text-gray-600 mb-2">Author: {book.author}</p>
+                <p className="text-gray-600 mb-2">Genre: {book.genre}</p>
+                <p className="text-gray-600">Publication Date: {book.publication_date}</p>
             </div>
          </div>)
            }
