0db6ef1918115eb3013100f211e566f78df565b7
mahabubr <mahabub.r753@gmail.com>
2023-07-15 17:02:00 +0600

    working on filter

--- src/pages/AllBooks/AllBooks.tsx
+++ src/pages/AllBooks/AllBooks.tsx
@@ -8,22 +8,88 @@ const AllBooks = () => {
 
     const {data}= useGetBooksQuery(searchTerm)
 
-
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
       setSearchTerm(event.target.value);
+
     };
 
+    const genre = Array.from(new Set(data?.data.map((g: string) => g.genre)));
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedGenre, setSelectedGenre] = useState('');
+  
+    const handleToggle = () => {
+      setIsOpen(!isOpen);
+    };
+
+    const handleGenreSelect = (selectedGenre: string) => {
+        setSelectedGenre(selectedGenre);
+        onGenreSelect(selectedGenre);
+        setIsOpen(false);
+      };
+    
+
+    const [year, setYear] = useState(1900);
+
+    const handleChange1 = (event: ChangeEvent<HTMLInputElement>) => {
+      setYear(Number(event.target.value));
+    };
 
     return (
         <div className="my-20 w-11/12 mx-auto">
         <div className="flex items-center justify-center w-full mb-10">
-            <input
-                type="text"
-                className="px-4 w-full py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={handleChange}
-            />
+            <div>
+                <input
+                    type="text"
+                    className="px-4 w-full py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="flex items-center gap-4">
+            <div className="dropdown">
+      <label
+        tabIndex={0}
+        className="btn m-1"
+        onClick={handleToggle}
+        onKeyDown={(e) => e.key === 'Enter' && handleToggle()}
+      >
+        Genre
+      </label>
+      {isOpen && (
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          {genre.map((g: string) => (
+            <li
+              key={g}
+              className="btn"
+              onClick={() => handleGenreSelect(g)}
+              onKeyDown={(e) => e.key === 'Enter' && handleGenreSelect(g)}
+            >
+              {g}
+            </li>
+          ))}
+        </ul>
+      )}
+      <p>Selected Genre: {selectedGenre}</p>
+    </div>
+                <div className="flex items-center gap-4">
+                    <input
+                        type="range"
+                        min={1900}
+                        max={2025}
+                        value={year}
+                        step={1}
+                        className="range"
+                        onChange={handleChange1}
+                    />
+                    
+                    <p>Selected Year: {year}</p>
+                </div>
+            </div>
             </div>
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {
